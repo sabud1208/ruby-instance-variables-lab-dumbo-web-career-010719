5cdbaf9a00a09c14c857cf044c06de00586789bc
Stephanie Abud <sabud1208@gmail.com>
2019-01-09 02:43:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
 class Dog
-
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
 end
-
-fido = Dog.new
-fido
-  
-snoopy = Dog.new
-snoopy
-
-lassie = Dog.new
-lassie
